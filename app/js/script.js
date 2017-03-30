5e20725868c434bff1064e4dca7ffb801975bae5
Vladimir Lipodat <sibi_7@mail.ru>
2017-03-30 11:04:21 +0300

    add animate scroll block

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
     $('.section-quote__slider').owlCarousel({
         loop:true,
         margin:10,
@@ -14,7 +16,25 @@ $(document).ready(function () {
                 items:3
             }
         }
-    })
+    });
+
+
+    $('.sideScroll').click(function (event) {
+        event.preventDefault(); // отменят стандартное свойство
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+        } else{
+            $('.sideScroll').removeClass('active'); // удаляет класс у всех дочерних элем.
+            $(this).addClass('active');
+        }
+        var href = $(this).attr('href'), // получает атрибут в конкретного  нажатого элем
+            target = $(href), // таргет- значение ссылки (ищет путь к элемменту на который указывает ссылка)
+            top = target.offset().top; // узнает на каком расстояние от верха был секция которую нуэно скроллить
+        $('html,body').animate({scrollTop: top}, 1000);
+        return false; // вышел из функции
+    });
+
+
 });
 
 
